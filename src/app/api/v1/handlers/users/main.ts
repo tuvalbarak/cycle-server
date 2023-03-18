21386d41013c01a92431f5387ee5936cfcf67a03
baraktuval@gmail.com <baraktuval@gmail.com>
2023-03-18 16:59:57 +0200

    iddd

--- src/app/api/v1/handlers/users/main.ts
+++ src/app/api/v1/handlers/users/main.ts
@@ -44,7 +44,7 @@ driversHandler.get('/',
  *
  */
 
-driversHandler.get('/:iddd', 
+driversHandler.get('/aaa:id', 
   asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
     const driverId = req.params.id;
 
