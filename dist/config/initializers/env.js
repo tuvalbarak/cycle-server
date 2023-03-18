87e7dc1d7dae964447e8cfad4f5b54dc6a15d8f8
baraktuval@gmail.com <baraktuval@gmail.com>
2023-03-18 22:34:02 +0200

    Dist update

--- dist/config/initializers/env.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// const env = process.env.NODE_ENV || 'development';
-const env = 'production';
-exports.default = env;
-//# sourceMappingURL=env.js.map
