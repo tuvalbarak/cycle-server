b5553db0576fee30cd79e6316dbf0f2678233daa
baraktuval@gmail.com <baraktuval@gmail.com>
2023-03-18 22:22:55 +0200

    Attempt to remove dist from deploy

--- dist/config/initializers/env.js
+++ dist/config/initializers/env.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const env = process.env.NODE_ENV || 'development';
+// const env = process.env.NODE_ENV || 'development';
+const env = 'production';
 exports.default = env;
 //# sourceMappingURL=env.js.map
