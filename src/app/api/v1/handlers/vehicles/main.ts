934837d04b2be966b2aa8d24fd47581de5d2948d
baraktuval@gmail.com <baraktuval@gmail.com>
2023-03-18 22:47:20 +0200

    get vehicles by id

--- src/app/api/v1/handlers/vehicles/main.ts
+++ src/app/api/v1/handlers/vehicles/main.ts
@@ -2,12 +2,13 @@ import { Router, Response, Request, NextFunction } from 'express';
 import { asyncHandler } from '../../helpers/routing';
 
 import VehicleMeta from '../../../../models/VehiclesMeta';
+import { ApiError, HttpStatus } from '../../helpers/error';
 
 const vehiclesMetaHandler = Router();
 
 /**
  * @swagger
- *  /Meta:
+ *  /vehiclesMeta:
  *  get:
  *    tags:
  *      - Vehicles Meta
@@ -22,6 +23,36 @@ vehiclesMetaHandler.get('/',
     const vehicles = <VehicleMeta[]>(await VehicleMeta.findAll());
 
     res.json({ code: 200, message: 'ok', data: vehicles });
+}));
+
+/**
+ * @swagger
+ *  /vehiclesMeta/{id}:
+ *  get:
+ *    tags:
+ *      - Vehicles Meta
+ *    parameters:
+ *      - name: id
+ *        schema:
+ *          type: integer
+ *        description: A valid vehicle id
+ *        required: true
+ *        in: path
+ *    responses:
+ *      200:
+ *        description: Ok
+ *
+ */
+
+vehiclesMetaHandler.get('/:id', 
+  asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const vehicleId = req.params.id;
+
+    const vehicle = await VehicleMeta.findByPk(vehicleId);
+
+    if (!vehicle) throw new ApiError(HttpStatus.BAD_REQUEST, `Vehicle ${vehicleId} not found`);
+
+    res.json({ code: 200, message: 'ok', data: vehicle });
 }));
 
 export default vehiclesMetaHandler
